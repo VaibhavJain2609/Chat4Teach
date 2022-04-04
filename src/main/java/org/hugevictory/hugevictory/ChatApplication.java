e03bf626744ad4437ce10cf1c8a97da52808c809
nikita.cherepanov <nikita.cherepanov02@gmail.com>
2022-04-03 21:29:21 -0700

    Springboot application files

---
+++ src/main/java/org/hugevictory/hugevictory/ChatApplication.java
@@ -0,0 +1,22 @@
+package org.hugevictory.hugevictory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class ChatApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChatApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
